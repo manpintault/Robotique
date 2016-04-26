ad68173ffecadfdf88e501d48687b7f158fb02f8
manpintault <pintault.manon@live.fr>
2016-04-26 11:48:22 +0200

    Update uselessFunction.py

--- uselessFunction.py
+++ uselessFunction.py
@@ -1,12 +1,12 @@
 import constant
-
+#function of initialization of the robot
 def initRobotIK():
 	coords = kinematics.computeDK(0, 0, 0)
 	angles = kinematics.computeIK(coords[0], coords[1], coords[2])
 
 	pos = dict(zip(constant.legs[0], angles))
 	constant.dxl_io.set_goal_position(pos)
-
+#function of initialisation of the position for all legs
 def initPos(legs):
 	for i in range(constant.NB_LEGS):
 		angles = constant.dxl_io.get_present_position(legs[i])
