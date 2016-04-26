83f252bde60f5809a9423454a9515f9629ae309a
manpintault <pintault.manon@live.fr>
2016-04-26 11:44:08 +0200

    maj

--- debug.py
+++ debug.py
@@ -1,6 +1,6 @@
 import constant
 import time
-
+#function for check the position of the leg
 def checkLeg(tab):
 	constant.dxl_io.set_goal_position({tab[0] : 0})
 	time.sleep(1)
@@ -8,14 +8,14 @@ def checkLeg(tab):
 	time.sleep(1)
 	constant.dxl_io.set_goal_position({tab[2] : 0})
 	time.sleep(1)
-
+#function for change an id by another
 def changeId(old, new):
 	constant.dxl_io.change_id({old: new})
 	for i in found_ids:
 		if(i == old):
 			found_ids[found_ids.index(old)] = new
 	print found_ids
-
+#function t check ids of the robot
 def checkId():
 	found_id = constant.dxl_io.scan()
 	print found_id
